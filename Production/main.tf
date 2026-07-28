3e26ae5f0d26221ff616e3eec79c92bea7a1419d
Amol Khilari <amol.khilari1231@gmail.com>
2026-07-28 19:37:18 +0530

    test code add

---
+++ Production/main.tf
@@ -0,0 +1,33 @@
+module "resource_group" {
+  source = "../module/resource_group"
+  rgs    = var.rgs
+}
+module "virtual_network" {
+
+  source     = "../module/Virtual_network"
+  vnets      = var.vnets
+  depends_on = [module.resource_group]
+}
+module "subnet" {
+  source     = "../module/Subnets"
+  subnets    = var.subnets
+  depends_on = [module.virtual_network]
+}
+module "pip" {
+  source     = "../module/Public_ip"
+  pips       = var.pips
+  depends_on = [module.resource_group]
+}
+
+module "vm" {
+  source = "../module/Virtual_machine"
+  vms    = var.vms
+  depends_on = [module.subnet, module.pip]
+}
+
+module "nat_gateways" {
+  source = "../module/NatGatway"
+  natgw =var.natgw
+  depends_on = [ module.subnet ]
+  
+}
---
+++ module/NatGatway/main.tf
@@ -0,0 +1,12 @@
+variable "natgw" {}
+
+resource "azurerm_nat_gateway" "natgw" {
+    for_each = var.natgw
+
+  name                    = each.value.name
+  location                = each.value.location
+  resource_group_name     = each.value.resource_group_name
+  sku_name                = each.value.sku_name
+  idle_timeout_in_minutes = each.value.idle_timeout_in_minutes
+  zones                   = each.value.zones
+}
